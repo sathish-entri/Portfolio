94f501f79353d251f899f029b4715cb738b3aa53
sathish-entri <abishsathish2002@example.com>
2024-10-11 22:15:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/preloader/Preloader";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/footer/Footer";
+// import { ThemeContext } from "./Context/theme";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/contact/Contact";
+ 
+function App() {
+  const [load, upadateLoad] = useState(true);
+  // const [{ themename }] = React.useContext(ThemeContext);
+ 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+
+  }, []);
+
+  return (
+    <div className={`app`}>
+   
+        <Preloader load={load} />
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
+          <Navbar />
+          <ScrollToTop />
+         
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/project" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+          <Footer />
+        </div>
+   
+    </div>
+  );
+}
+
+export default App;
+// "homepage": "https://sathish-entri.github.io/SathishPortfolio",
+// " predeploy": "npm run build",
+// "deploy": "gh-pages -d build",
+// "homepage": "https://sathish-entri.github.io/SathishPortfolio",
+// " predeploy": "npm run build",
+// "deploy": "gh-pages -d build",
